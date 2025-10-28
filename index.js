71fae36f3b8644ea20d1e4074509b53a2859202d
Vaibhav Dudhal <vaibhavdudhal11@gmail.com>
2025-10-28 18:07:04 +0000

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello from Node + Kubernetes + ArgoCD!</h1>");
+  res.send("<h1>Hello from Node + Kubernetes + ArgoCD!  from Vaibhav D </h1>");
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
